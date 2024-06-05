648c1c9fd4056f75dd32227cba00a32023fcd794
Steve Boyd <emteknetnz@gmail.com>
2024-06-05 14:57:12 +1200

    .

--- run.php
+++ run.php
@@ -10,30 +10,64 @@ $accounts = [
     'tractorcow',
 ];
 
-// function to recursively scan php files in vendor/<account>
-function getPhpFiles($path, &$files) {
+$patterns = [
+    'self::',
+    ': self',
+    '@return self',
+    'new self('
+];
+
+function php_files($path, &$files = []) {
     if (!is_dir($path)) {
         return;
     }
     $items = scandir($path);
     foreach ($items as $item) {
-        if ($item === '.' || $item === '..') {
+        if ($item === '.' || $item === '..' || $item === '.git') {
             continue;
         }
         $fullPath = "$path/$item";
         if (is_dir($fullPath)) {
-            getPhpFiles($fullPath, $files);
+            php_files($fullPath, $files);
         } else if (pathinfo($fullPath, PATHINFO_EXTENSION) === 'php') {
             $files[] = $fullPath;
         }
     }
 }
 
-// loop directories in vendor/<account>
+$rootDir = __DIR__;
+for ($i = 0; $i < 5; $i++) {
+    if (!file_exists("$rootDir/.env")) {
+        $rootDir = dirname($rootDir);
+    } else {
+        echo "Root dir is $rootDir\n";
+        break;
+    }
+}
+
+$reposUpdated = [];
+
 foreach ($accounts as $account) {
-    $path = "vendor/$account";
-    $files = [];
-    $files = getPhpFiles($path, $files);
-    print_r($files);
+    $path = "$rootDir/vendor/$account";
+    php_files($path, $files);
+    foreach ($files as $file) {
+        preg_match("#^$path/(.+?)/#", $file, $m);
+        if (!$m) {
+            // file isn't in the current account
+            continue;
+        }
+        $repo = $m[1];
+        $shortName = basename($file, '.php');
+        $c = file_get_contents($file);
+        $orig = $c;
+        foreach ($patterns as $pattern) {
+            $replacement = str_replace('self', $shortName, $pattern);
+            $c = str_replace($pattern, $replacement, $c);
+            if ($c != $orig) {
+                file_put_contents($file, $c);
+                echo "Updated $file\n";
+            }
+        }
+    }
     echo "Scanned $path\n";
 }
